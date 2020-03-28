ad44ff90eb9601a54f429c72fdf4e3095255c4ff
zhigang.xu <zhigang@mamahao.com>
2020-03-28 12:56:48 +0800

    Eureka服务注册与发现，自我保护机制

--- README.md
+++ README.md
@@ -23,3 +23,12 @@ springcloud_demo
 - 微服务RPC远程调用最核心的是什么?
     1. 高可用 如果只有一个注册中心 宕机会导致整个微服务环境不可用
     2. 搭建Eureka注册中心集群 实现负载均衡+故障容器
+    
+## Eureka自我保护
+**概述**
+1. 保护模式主要用于一组客户端和Eureka Server之间存在网络分区场景下的保护。一旦进入保护模式，
+Eureka Server将会尝试保护服其服务注册表中的信息，不再删除服务注册表中的数据。也就是不会注销任何微服务。
+
+如果Eureka Server的首页看到以下这段提示，则说明Eureka进入了保护模式
+> EMERGENCY! EUREKA MAY BE INCORRECTLY CLAIMING INSTANCES ARE UP WHEN THEY'RE NOT. RENEWALS ARE LESSER THAN THRESHOLD AND HENCE THE INSTANCES ARE NOT BEING EXPIRED JUST TO BE SAFE.
+  DS Replicas
--- cloud-provider-payment8001/src/main/java/com/springcloud/controller/PaymentController.java
+++ cloud-provider-payment8001/src/main/java/com/springcloud/controller/PaymentController.java
@@ -5,9 +5,12 @@ import com.springcloud.entities.Payment;
 import com.springcloud.service.PaymentService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
+import java.util.List;
 
 /**
  * @author zhigang.xu
@@ -20,6 +23,9 @@ public class PaymentController {
 
     @Value("${server.port}")
     private String serverPort;
+
+    @Resource
+    private DiscoveryClient discoveryClient;
 
     @PostMapping("/payment/create")
     public CommonResult create(@RequestBody Payment payment) {
@@ -42,5 +48,20 @@ public class PaymentController {
         } else {
             return new CommonResult(444, "查询失败");
         }
+    }
+
+    @GetMapping("/payment/discovery")
+    public Object discovery() {
+        List<String> services = discoveryClient.getServices();
+        for (String element : services) {
+            log.info("*********element: {}", element);
+        }
+
+        List<ServiceInstance> instances = discoveryClient.getInstances("CLOUD-PAYMENT-SERVICE");
+        for (ServiceInstance instance : instances) {
+            log.info(instance.getServiceId()+"\t"+instance.getHost()+"\t"+instance.getPort()+"\t"+instance.getUri());
+        }
+
+        return this.discoveryClient;
     }
 }
